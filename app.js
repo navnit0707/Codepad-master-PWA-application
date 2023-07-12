6ea5e2a3eb698851a2ec184cee0d5073ecec1bc2
navnit0707 <knavnit81@gmail.com>
2023-07-13 01:10:09 +0530

    manfiest update

--- app.js
+++ app.js
@@ -20,25 +20,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   });
 
-  document.querySelector("#btnLearn").addEventListener("click", (event) => {
-    location.href = "https://frontendmasters.com";
-  });
   let bipEvent = null;
   window.addEventListener("beforeinstallprompt", (event) => {
     event.preventDefault();
     bipEvent = event;
   });
+
   document.querySelector("#btnInstall").addEventListener("click", (event) => {
     if (bipEvent) {
       bipEvent.prompt();
     } else {
+      // incompatible browser, your PWA is not passing the criteria, the user has already installed the PWA
+      //TODO: show the user information on how to install the app
       alert(
-        "to download the app look for add to homescreen or add to shortcut"
+        "To install the app look for Add to Homescreen or Install in your browser's menu"
       );
     }
   });
+
   document.querySelector("#btnShare").addEventListener("click", (event) => {
-    console.log("share");
     let notesString = "";
     for (let note of notes) {
       notesString += note + " | ";
